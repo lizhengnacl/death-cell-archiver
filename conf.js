68b9ed43f87cc9c1d28549245cc463947d7f4330
lizhengnacl <lizhengnacl@163.com>
2020-08-15 11:27:01 +0800

    fix: resolve path

--- conf.js
+++ conf.js
@@ -1,16 +1,17 @@
+const path = require('path');
 const YAML = require('yaml');
 const fs = require('fs');
 
 class Config {
   read() {
-    return YAML.parse(fs.readFileSync('./conf.yml', 'utf8'));
+    return YAML.parse(fs.readFileSync(path.resolve(__dirname, './conf.yml'), 'utf8'));
   }
 
   set(path) {
     let conf = this.read();
     conf.path = path;
     let content = YAML.stringify(conf);
-    fs.writeFileSync('./conf.yml', content, 'utf8');
+    fs.writeFileSync(path.resolve(__dirname, './conf.yml'), content, 'utf8');
   }
 }
 
